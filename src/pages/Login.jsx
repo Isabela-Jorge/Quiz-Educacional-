4202c4c9c32d4ecb38d029a94df02e3884f9bf70
Aluno <e023aluno@outlook.com>
2026-09-23 22:03:08 -0300

    feat: inicia estrutura do frontend

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-md">
+        
+        <h1 className="text-3xl font-bold text-center">
+          QUIZ EDUCATIVO
+        </h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
